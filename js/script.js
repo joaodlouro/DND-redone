76934828e9de5f8838424dcf0c474d88fdafa00f
joaovx1315 <joaovitorvx1315@gmail.com>
2025-05-04 15:44:58 -0300

    cards add

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
-import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+var limits = 15.0;
 
-// Executa tudo **apenas após o carregamento do DOM**
-if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-  document.addEventListener('DOMContentLoaded', () => {
-    initScrollSuave();
-    initAnimacaoScroll();
-    initAccordion();
-    initTabNav();
-    initModal();
-    initTooltip();
-    initDropdownMenu();
-    initMenuMobile();
-    initFuncionamento();
-    initFetchAnimais();
-    initFetchBitcoin();
+$(".card").mousemove(function (e) {
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top; 
+  var offsetX = x / rect.width;
+  var offsetY = y / rect.height;
+
+  var rotateY = (offsetX) * (limits * 2) - limits;
+  var rotateX = (offsetY) * (limits * 2) - limits;
+
+  var shadowOffsetX = (offsetX) * 32 - 16;
+  var shadowOffsetY = (offsetY) * 32 - 16;
+
+  $(this).css({
+    "box-shadow": (1 / 6) * -shadowOffsetX + "px " + (1 / 6) * -shadowOffsetY + "px 3px rgba(0, 0, 0, 0.051), " +
+      (2 / 6) * -shadowOffsetX + "px " + (2 / 6) * -shadowOffsetY + "px 7.2px rgba(0, 0, 0, 0.073), " +
+      (3 / 6) * -shadowOffsetX + "px " + (3 / 6) * -shadowOffsetY + "px 13.6px rgba(0, 0, 0, 0.09), " +
+      (4 / 6) * -shadowOffsetX + "px " + (4 / 6) * -shadowOffsetY + "px 24.3px rgba(0, 0, 0, 0.107), " +
+      (5 / 6) * -shadowOffsetX + "px " + (5 / 6) * -shadowOffsetY + "px 45.5px rgba(0, 0, 0, 0.129), " +
+      -shadowOffsetX + "px " + -shadowOffsetY + "px 109px rgba(0, 0, 0, 0.18)",
+    transform: "perspective(1000px) rotateX(" + -rotateX + "deg) rotateY(" + rotateY + "deg)"
   });
-}
+
+  var glarePos = rotateX + rotateY + 90;
+  $(this)
+    .children()
+    .children()
+    .css("left", glarePos + "%");
+});
+
+$(".card").mouseleave(function (e) {
+    $(".card").css({"box-shadow": "0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)", "transform": "scale(1.0)"});
+    $(".glare").css("left", "100%");
+  });
